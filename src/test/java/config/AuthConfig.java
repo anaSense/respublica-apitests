57005215df606df6d87545263742c006cb90ee48
annaegorova <anna-khabarova2107@yandex.ru>
2024-05-03 23:25:43 +0300

    fixed tests configuration

--- src/test/java/config/AuthConfig.java
+++ src/test/java/config/AuthConfig.java
@@ -2,7 +2,9 @@ package config;
 
 import org.aeonbits.owner.Config;
 
+@Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
+        "system:properties",
         "classpath:properties/auth.properties"
 })
 public interface AuthConfig extends Config {
